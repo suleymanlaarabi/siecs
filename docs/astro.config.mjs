79113e416fbeca0b502ad7c25a17f7c0edb8cd54
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-06-07 21:27:20 +0200

    doc: add github page for documentation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://suleymanlaarabi.github.io',
+  base: '/siecs',
   integrations: [
     starlight({
       title: 'SIECS',
