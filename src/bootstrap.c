eda5cf2035ea8ed26eee021835c8780588ca7457
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-06-08 10:40:34 +0200

    doc: clarify id reserve

--- src/bootstrap.c
+++ src/bootstrap.c
@@ -4,13 +4,9 @@
 ECS_RELATION_DEFINE(ChildOf);
 
 void ecs_bootstrap(ecs_world_t *world) {
-    // Create empty table (index 0)
+    // Reserve identifiers used to represent false return values.
     ecs_table_index_get_or_create(world, (ecs_type_t){ 0 });
-
-    // Reserve entity id 0
     ecs_new(world);
-
-    // Reserve component id 0
     ecs_component(world, {});
 
     ECS_COMPONENT_REGISTER(world, ChildOf);
