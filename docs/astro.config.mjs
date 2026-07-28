d2fefac29be035e423446b8062e504daa32c45a8
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-07-28 03:45:22 +0200

    remove internal world documentation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -42,7 +42,7 @@ export default defineConfig({
         },
         {
           label: 'Production',
-          items: ['performance', 'internals'],
+          items: ['performance'],
         },
         {
           label: 'Reference',
