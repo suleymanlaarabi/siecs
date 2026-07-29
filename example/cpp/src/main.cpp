871c43ded434998f8377aa832913b85c6ff0ff8c
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-07-29 08:09:19 +0200

    fix cpp example build on clang18

--- example/cpp/src/main.cpp
+++ example/cpp/src/main.cpp
@@ -1,13 +1,7 @@
-#include "siecs/cpp/entity.hpp"
-#include "siecs/cpp/query.hpp"
-#include "siecs/cpp/resource.hpp"
-#include "siecs/cpp/system.hpp"
-#include "siecs/cpp/world.hpp"
 #include <concepts>
 #include <cstdint>
 #include <cstdio>
 #include <iostream>
-#include <print>
 #include <siecs.h>
 
 #include <cassert>
@@ -31,10 +25,6 @@ struct NoIntegrate {};
 int main() {
     ecs::init({ .rest = false, .target_fps = 60 });
 
-    std::println("{}", ecs::entity::lookup("Enemy").id());
-
     ecs::entity::create<Enemy>();
     ecs::entity::create("Enemy");
-
-    std::println("{}", ecs::entity::lookup("Enemy").id());
 }
